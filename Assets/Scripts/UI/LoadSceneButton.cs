b03c824296d9f132bbc3e6bd44be8b5168945ab1
ElmeriR <elmeri.rusi@edu.turkuamk.fi>
2025-04-16 13:10:48 +0300

    Add new shaders and assets for TextMesh Pro integration
    - Created new shader files: TMPro_Properties.cginc and TMPro_Surface.cginc with vertex and pixel shader implementations.
    - Added metadata files for the new shaders.
    - Introduced EmojiOne sprite assets including attribution text, JSON metadata, and PNG images.
    - Updated manifest to include the new input system package.
    - Modified project settings to include new scenes and adjust input handler settings.
    - Added SceneTemplateSettings for better scene management.

---
+++ Assets/Scripts/UI/LoadSceneButton.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RTS.Runtime
+{
+    [RequireComponent(typeof(UnityEngine.UI.Button))]
+    public class LoadSceneButton : MonoBehaviour
+    {
+        [SerializeField] private int _sceneToLoad; // The index of the scene to load
+        private UnityEngine.UI.Button _button; // Reference to the button component
+
+        private void Awake()
+        {
+            // Get the button component and add a listener to it
+            _button = GetComponent<UnityEngine.UI.Button>();
+            UnityEngine.Assertions.Assert.IsNotNull(_button, "Button component is missing on this GameObject.");
+            _button.onClick.AddListener(LoadScene);
+        }
+
+        public void LoadScene()
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(_sceneToLoad);
+        }
+    }
+    
+}
